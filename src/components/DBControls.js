c5190fd2d67cde031861db0562707a769b0ac750
GC <gcappai.dev@gmail.com>
2022-12-13 17:09:22 +0100

    Refactor DB controls to access global store

--- src/components/DBControls.js
+++ src/components/DBControls.js
@@ -1,20 +1,32 @@
-import { clearDB } from "../lib/db";
+import { showDS } from "../context/actions";
+import { usePurchase } from "../context/Purchase";
+import * as db from "../lib/db";
 
-function DBControls({ setDSVisible }) {
+function DBControls() {
+  const { dispatch } = usePurchase();
+
+  const handleOpenDailySales = () => {
+    dispatch(showDS());
+  };
+
+  const handleClearAll = () => {
+    db.clearDB();
+  };
+
   return (
     <div id="db_controls" className="mt-4 card">
       <form className="flex ">
         <button
           type="button"
           className="flex-1 text-sm focus:outline-none uppercase text-blue-500 font-semibold tracking-wide py-2 mr-4 hover:bg-blue-50 rounded-lg border-2 border-blue-500"
-          onClick={() => setDSVisible((v) => !v)}
+          onClick={handleOpenDailySales}
         >
           Daily Sales
         </button>
         <button
           type="button"
           className="flex-1 text-sm focus:outline-none uppercase text-red-500 font-semibold  tracking-wide py-2 hover:bg-red-50 rounded-lg border-2 border-red-500"
-          onClick={() => clearDB()}
+          onClick={handleClearAll}
         >
           Clear All
         </button>
