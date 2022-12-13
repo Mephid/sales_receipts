520b551bccec82f64d1bc73f27008955c5e7fb4a
GC <gcappai.dev@gmail.com>
2022-12-13 16:14:02 +0100

    Move App layout into separate component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import { useState } from "react";
-
 import "./App.css";
 
-import ReceiptPanel from "./components/ReceiptPanel";
-import PurchasePanel from "./components/PurchasePanel";
-import DBControls from "./components/DBControls";
-import DailySales from "./components/DailySales";
-import { PurchaseProvider } from "./context/Purchase";
+import MainView from "./views/main";
 
 function App() {
-  //Visibility state of Daily Sales
-  const [isDSVisible, setDSVisible] = useState(false);
-
-  //CHANGE COLUMN RATIO
-  return (
-    <div id="app_container" className="bg-gray-100 h-screen w-screen p-6">
-      <PurchaseProvider>
-        <div className="flex h-full">
-          <div className="h-full mr-2 flex flex-col">
-            <ReceiptPanel />
-            <DBControls setDSVisible={setDSVisible} />
-          </div>
-          <div className="">
-            <PurchasePanel />
-          </div>
-        </div>
-      </PurchaseProvider>
-      {isDSVisible && <DailySales setVisibility={setDSVisible} />}
-    </div>
-  );
+  return <MainView />;
 }
 
 export default App;
