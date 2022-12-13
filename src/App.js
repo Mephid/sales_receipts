79607fdee20027a80334c3214fce6261abeb6da0
GC <gcappai.dev@gmail.com>
2022-12-13 17:06:34 +0100

    Remove comment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import MainView from "./views/Main";
 
 function App() {
-  //TODO: ADD react router to open/close DailiSalesView
   return (
     <div id="app_container" className="bg-gray-100 h-screen w-screen p-6">
       <MainView />
