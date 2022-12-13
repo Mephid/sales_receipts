ba3a799718ff9b95e42147592146fc1bac68ee0d
GC <gcappai.dev@gmail.com>
2022-12-13 18:51:58 +0100

    Move provider to App level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import { PurchaseProvider } from "./context/Purchase";
 
 import MainView from "./views/Main";
 
 function App() {
   return (
     <div id="app_container" className="bg-gray-100 h-screen w-screen p-6">
-      <MainView />
+      <PurchaseProvider>
+        <MainView />
+      </PurchaseProvider>
     </div>
   );
 }
